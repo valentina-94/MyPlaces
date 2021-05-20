231fb94cb721b83f6dd34dc3dd46c535db0e27c4
valentina-94 <ivanenkovavalya@rambler.ru>
2021-05-20 19:00:12 +0300

    Design for NewPlaceViewController

---
+++ MyPlaces/NewPlaceViewController.swift
@@ -0,0 +1,43 @@
+//
+//  NewPlaceViewController.swift
+//  MyPlaces
+//
+//  Created by Валентина Викулова on 20.05.2021.
+//
+
+import UIKit
+
+class NewPlaceViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.tableFooterView = UIView()
+
+        
+    }
+    
+    // MARK: Table view delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if indexPath.row == 0 {
+            
+        } else {
+            view.endEditing(true)
+        }
+    }
+
+}
+
+// MARK: Text field delegate
+
+extension NewPlaceViewController: UITextFieldDelegate {
+    
+    //Скрываем клавиатуру по нажатию на Done
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+}
